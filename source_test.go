977dc5588e858579e38f2b94148e51ca0b1fd7f8
Rosie Kern <rk18@sanger.ac.uk>
2025-07-28 16:03:22 +0100

    Improve tests

--- source_test.go
+++ source_test.go
@@ -10,6 +10,8 @@ import (
 	"github.com/gocarina/gocsv"
 )
 
+const numTestDataRows = 3
+
 func TestReadAll(t *testing.T) {
 	originalEntries, testPath := createTestData(t)
 
@@ -63,38 +65,31 @@ func TestUpdateEntry(t *testing.T) {
 }
 
 func TestDeleteEntry(t *testing.T) {
-	var tests = []struct {
-		idToDelete uint16
-	}{
-		{0},
-		{1},
-		{2},
-	}
+	for id := range numTestDataRows {
+		idToDelete := uint16(id)
 
-	for _, tt := range tests {
-		testname := fmt.Sprintf("Entry %d", tt.idToDelete)
-		t.Run(testname, func(t *testing.T) {
-			originalEntries, testPath := createTestData(t)
-		
+		t.Run(fmt.Sprintf("Entry %d", idToDelete), func(t *testing.T) {
+			entriesBefore, testPath := createTestData(t)
+
 			csvSource := CSVSource{path: testPath}
 
-			err := csvSource.deleteEntry(tt.idToDelete)
+			err := csvSource.deleteEntry(idToDelete)
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			entries, err := csvSource.readAll()
+			entriesAfter, err := csvSource.readAll()
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			if len(entries) != (len(originalEntries) - 1) {
+			if len(entriesAfter) != (len(entriesBefore) - 1) {
 				t.Errorf("CSV has the wrong number of entries.\nGot %+v, expected %+v",
-					entries[0], originalEntries[0])
+					entriesAfter[0], entriesBefore[0])
 			}
 
-			for _, e := range entries {
-				if e.ID == tt.idToDelete {
+			for _, e := range entriesAfter {
+				if e.ID == idToDelete {
 					t.Errorf("Deleted entry still present: %+v", e)
 				}
 			}
@@ -115,7 +110,7 @@ func createTestData(t *testing.T) ([]Entry, string) {
 	}
 
 	var entries []Entry
-	for i := range 3 {
+	for i := range numTestDataRows {
 		newEntry := baseEntry
 		newEntry.ReportingName = fmt.Sprintf("test_project_%d", i)
 		newEntry.ID = uint16(i)
