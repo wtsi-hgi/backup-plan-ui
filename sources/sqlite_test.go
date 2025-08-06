1b06796f30c4540e3d1e1c0372f758c938e7cad6
Iaroslav Popov <ip13@sanger.ac.uk>
2025-08-06 13:08:45 +0100

    Finish SQLiteSource implementation

--- sources/sqlite_test.go
+++ sources/sqlite_test.go
@@ -8,6 +8,76 @@ import (
 	. "github.com/smarty/assertions"
 )
 
+func TestSQLiteSource_ReadAll(t *testing.T) {
+	entries, sq := createTestTable(t)
+	defer sq.Close()
+
+	testDataSourceReadAll(t, sq, entries)
+}
+
+func TestSQLiteSource_GetEntry(t *testing.T) {
+	entries, sq := createTestTable(t)
+	defer sq.Close()
+
+	testDataSourceGetEntry(t, sq, entries)
+}
+
+func TestSQLiteSource_UpdateEntry(t *testing.T) {
+	entries, sq := createTestTable(t)
+	defer sq.Close()
+
+	testDataSourceUpdateEntry(t, sq, entries)
+}
+
+func TestSQLiteSource_DeleteEntry(t *testing.T) {
+	testCases := []struct {
+		name    string
+		entryID uint16
+	}{
+		{"Delete first entry", 1},
+		{"Delete middle entry", max(1, NumTestDataRows-1)},
+		{"Delete last entry", NumTestDataRows},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			entries, sq := createTestTable(t)
+			defer sq.Close()
+
+			testDataSourceDeleteEntry(t, sq, entries[tt.entryID-1], tt.entryID)
+		})
+	}
+}
+
+func TestSQLiteSource_AddEntry(t *testing.T) {
+	entries, sq := createTestTable(t)
+	defer sq.Close()
+
+	testDataSourceAddEntry(t, sq, entries)
+}
+
+func TestSQLiteSource_WriteEntries(t *testing.T) {
+	entries := CreateTestEntries(t)
+
+	dbPath := filepath.Join(t.TempDir(), "test.db")
+
+	sq, err := NewSQLiteSource(dbPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer sq.Close()
+
+	err = sq.CreateTable()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = sq.writeEntries(entries)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestCreateTable(t *testing.T) {
 	dbFile := filepath.Join(t.TempDir(), "test.db")
 
@@ -43,40 +113,4 @@ func TestCreateTable(t *testing.T) {
 	if ok, err := So(tableNames, ShouldContain, entriesTableName); !ok {
 		log.Fatal(err)
 	}
-}
-
-func TestSQLiteSource_WriteEntries(t *testing.T) {
-	entries := CreateTestEntries(t)
-
-	dbPath := filepath.Join(t.TempDir(), "test.db")
-
-	sq, err := NewSQLiteSource(dbPath)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer sq.Close()
-
-	err = sq.CreateTable()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = sq.writeEntries(entries)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestSQLiteSource_ReadAll(t *testing.T) {
-	originalEntries, sq := createTestTable(t)
-	defer sq.Close()
-
-	testDataSourceReadAll(t, sq, originalEntries)
-}
-
-func TestSQLiteSource_GetEntry(t *testing.T) {
-	originalEntries, sq := createTestTable(t)
-	defer sq.Close()
-
-	testDataSourceGetEntry(t, sq, originalEntries)
 }
