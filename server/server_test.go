064612dc8cf99128c376ab49641c0325608473c2
Rosie Kern <160497849+rk1274@users.noreply.github.com>
2025-08-06 15:02:51 +0100

    Add text when hovering over table headers (#15)
    * Add tooltip for table headers
    
    * Move tooltip below header
    
    * Add tooltip to add row table too
    
    * Add server test for delete row
    
    * Remove unndeeded value
    
    * Improve tooltip styling
    
    * Add more meaningful tests for deleteRow

--- server/server_test.go
+++ server/server_test.go
@@ -174,19 +174,81 @@ func TestSubmitEdits(t *testing.T) {
 	}
 }
 
-func createFormFromEntry(entry sources.Entry) url.Values {
-	form := make(url.Values)
+func TestDeleteRow(t *testing.T) {
+	s, originalEntries := createServer(t)
 
-	form.Set(ReportingName.string(), entry.ReportingName)
-	form.Set(ReportingRoot.string(), entry.ReportingRoot)
-	form.Set(Directory.string(), entry.Directory)
-	form.Set(Instruction.string(), string(entry.Instruction))
-	form.Set(Match.string(), entry.Match)
-	form.Set(Ignore.string(), entry.Ignore)
-	form.Set(Requestor.string(), entry.Requestor)
-	form.Set(Faculty.string(), entry.Faculty)
+	t.Run("You can delete a valid entry", func(t *testing.T) {
+		entry := originalEntries[0]
 
-	return form
+		w := httptest.NewRecorder()
+		r := makeRequest(entry.ID)
+
+		s.DeleteRow(w, r)
+
+		_ = getBodyAndCheckStatusOK(t, w)
+
+		entries, err := s.db.ReadAll()
+		if err != nil {
+			t.Error(err)
+		}
+
+		if ok, err := So(entries, ShouldNotContain, entry); !ok {
+			t.Error(err)
+		}
+	})
+
+	t.Run("You must provide a valid ID", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		ctx := chi.NewRouteContext()
+		ctx.URLParams.Add("id", "invalid ID")
+
+		r = r.WithContext(context.WithValue(r.Context(), chi.RouteCtxKey, ctx))
+
+		s.DeleteRow(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		if ok, err := So(res.StatusCode, ShouldEqual, http.StatusBadRequest); !ok {
+			t.Error(err)
+		}
+	})
+
+	t.Run("You must provide an ID currently in use", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		ctx := chi.NewRouteContext()
+		ctx.URLParams.Add("id", fmt.Sprint(sources.NumTestDataRows))
+
+		r = r.WithContext(context.WithValue(r.Context(), chi.RouteCtxKey, ctx))
+
+		s.DeleteRow(w, r)
+
+		res := w.Result()
+		defer res.Body.Close()
+
+		if ok, err := So(res.StatusCode, ShouldEqual, http.StatusInternalServerError); !ok {
+			t.Error(err)
+		}
+
+		errMsg, err := io.ReadAll(res.Body)
+		if ok, err := So(err, ShouldBeNil); !ok {
+			t.Fatal(err)
+		}
+
+		if ok, err := So(string(errMsg), ShouldContainSubstring, sources.ErrNoEntry.Error()); !ok {
+			t.Error(err)
+		}
+	})
+}
+
+func makeRequest(id uint16) *http.Request {
+	r := httptest.NewRequest(http.MethodGet, "/", nil)
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", fmt.Sprint(id))
+
+	return r.WithContext(context.WithValue(r.Context(), chi.RouteCtxKey, ctx))
 }
 
 func TestValidateForm(t *testing.T) {
@@ -277,6 +339,21 @@ func TestValidateForm(t *testing.T) {
 			}
 		})
 	}
+}
+
+func createFormFromEntry(entry sources.Entry) url.Values {
+	form := make(url.Values)
+
+	form.Set(ReportingName.string(), entry.ReportingName)
+	form.Set(ReportingRoot.string(), entry.ReportingRoot)
+	form.Set(Directory.string(), entry.Directory)
+	form.Set(Instruction.string(), string(entry.Instruction))
+	form.Set(Match.string(), entry.Match)
+	form.Set(Ignore.string(), entry.Ignore)
+	form.Set(Requestor.string(), entry.Requestor)
+	form.Set(Faculty.string(), entry.Faculty)
+
+	return form
 }
 
 func createServer(t *testing.T) (Server, []*sources.Entry) {
